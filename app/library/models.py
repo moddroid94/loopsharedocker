ca8174a0f81ff6a80d5bbd7da6284aeabfaa80c2
moddroid94 <ilsecco94@gmail.com>
2024-03-18 23:27:50 +0100

    Implemented dynamic save path for samples

--- app/library/models.py
+++ app/library/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.conf import settings
 
+import logging
 
+logger = logging.getLogger(__name__)
 # Create your models here.
 
 def get_pack_types():
     return {i: x for i,x in settings.PACKTYPES}
+
+def get_save_path(instance, filename):
+    logging.warning(instance.pack)
+    return 'uploads/{0}/Sounds/{1}'.format(instance.pack, filename)
 
 class Pack(models.Model):
     type = models.CharField(max_length=3, choices=get_pack_types)
@@ -17,5 +23,8 @@ class Pack(models.Model):
     
 class Sample(models.Model):
     pack = models.ForeignKey(Pack, on_delete=models.CASCADE,related_name="samples")
-    file = models.FileField(upload_to='uploads/sounds/')
+    file = models.FileField(upload_to=get_save_path)
+
+    def __str__(self):
+        return f"{self.file}"
 
